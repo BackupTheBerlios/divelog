b1ca3d68030f83299bd9fd6e0209a05de63d7eea
markus <markus>
2001-11-14 16:13:19 +0000

    Hip Hip :) Got my first mysql-response ...

--- divelog/DivelogDAO.cpp
+++ divelog/DivelogDAO.cpp
@@ -1,6 +1,6 @@
 /******************************************************************************
 * Filename : DivelogDAO.cpp                                                   *
-* CVS Id   : $Id: DivelogDAO.cpp,v 1.4 2001/11/13 19:25:53 markus Exp $       *
+* CVS Id   : $Id: DivelogDAO.cpp,v 1.5 2001/11/14 16:13:19 markus Exp $       *
 * --------------------------------------------------------------------------- *
 * Files subject    : Data Access Object (DAO) for the mysql-divelog database  *
 * Owner            : Markus Grunwald (MG)                                     *
@@ -11,7 +11,7 @@
 * --------------------------------------------------------------------------- *
 * Notes :                                                                     *
 ******************************************************************************/
-static char *DivelogDAO_cvs_id="$Id: DivelogDAO.cpp,v 1.4 2001/11/13 19:25:53 markus Exp $";
+static char *DivelogDAO_cvs_id="$Id: DivelogDAO.cpp,v 1.5 2001/11/14 16:13:19 markus Exp $";
 #include "divelogdao.h"
 //#include <iostream>   // first see, what we need...
 //#include <iomanip>    // dito
@@ -52,6 +52,10 @@ void DivelogDAO::importUDCFFile( const char* filename )
 
     int s = strlen( filename );
     char* test= new char[s];
+    strcpy( test, filename );
+
+
+    qDebug( "test=%s", test );
 
     UDCF* udcfData=UDCFReadFile( test );
     CHECK_PTR( udcfData );
@@ -70,38 +74,81 @@ void DivelogDAO::importUDCFFile( const char* filename )
     || First look up if we know that dive-computer
     */
 
-    /*FIXME: That damn b...t doesn't work. Why ????
-
-    Query query = m_con->query();
-    query << "select * from divecomputer where serial_number=" << udcfData->serialID;
-
-    Result db_diveComputers = query.store(); // Database result
-
-    for ( i=db_diveComputers.begin(); i!=db_diveComputers.end() ;i++ )
+    try
     {
-    qDebug( "mysql: divecomputer serial number =%s", i->serial_number.c_str() );
-    qDebug( "mysql: divecomputer diver  number =%d", i->diver_number );
-    qDebug( "mysql: divecomputer name          =%s", i->name.c_str() );
-    }
-    */
-    int count=0;
-
-    for ( int group=0; group<=udcfData->groupIndex; group++)
-    {
-        for ( int dive=0; dive<=udcfData->groupList[group].diveIndex; dive++ )
+        Connection con( use_exceptions );
+        con.connect( MYSQL_DATABASE, MYSQL_HOST, MYSQL_USER, MYSQL_PASSWD );
+        // Here I broke making the connection into two calls.
+        // The first one creates the Connection object with the 
+        // use exceptions option turned on and the second one
+        // makes the connection
+        
+        Query query = con.query();
+        query << "select * from divecomputer where serial_number=\"" << udcfData->serialID <<"\"";
+    
+        Result db_diveComputers = query.store(); // Database result
+    
+        /*FIXME: That damn b...t doesn't work. Why ????
+                 I.e. : dynamically creating the Connection object gives
+                 strange errors...
+        */
+    
+        Row row;
+    
+        Result::iterator i;
+        for ( i=db_diveComputers.begin(); i!=db_diveComputers.end() ;i++ )
         {
-
-            qDebug( "#%04d Group[%d].diveList[%d] %02d.%02d.%04d %02d:%02d",
-                    count, group, dive,
-                    udcfData->groupList[group].diveList[dive].day,
-                    udcfData->groupList[group].diveList[dive].month,
-                    udcfData->groupList[group].diveList[dive].year,
-                    udcfData->groupList[group].diveList[dive].hour,
-                    udcfData->groupList[group].diveList[dive].minute );
-
-            count++;
-
+            row=*i;
+            qDebug( "mysql: divecomputer serial number =%s", row["serial_number"].c_str() );
+            qDebug( "mysql: divecomputer diver  number =%d", row["diver_number"] );
+            qDebug( "mysql: divecomputer name          =%s", row["name"].c_str() );
         }
+    
+        int count=0;
+    
+        for ( int group=0; group<=udcfData->groupIndex; group++)
+        {
+            for ( int dive=0; dive<=udcfData->groupList[group].diveIndex; dive++ )
+            {
+    
+                qDebug( "#%04d Group[%d].diveList[%d] %02d.%02d.%04d %02d:%02d",
+                        count, group, dive,
+                        udcfData->groupList[group].diveList[dive].day,
+                        udcfData->groupList[group].diveList[dive].month,
+                        udcfData->groupList[group].diveList[dive].year,
+                        udcfData->groupList[group].diveList[dive].hour,
+                        udcfData->groupList[group].diveList[dive].minute );
+    
+                count++;
+    
+            }
+        }
+        UDCFFree( udcfData );
     }
-    UDCFFree( udcfData );
+    catch (BadQuery &er)
+    {   // handle any connection or
+        // query errors that may come up
+#ifdef USE_STANDARD_EXCEPTION
+        cerr << "Error: " << er.what() << endl;
+#else
+        cerr << "Error: " << er.error << endl;
+#endif
+    }
+    catch (BadConversion &er)
+    { // handle bad conversions
+#ifdef USE_STANDARD_EXCEPTION
+        cerr << "Error: " << er.what() << "\"." << endl
+            << "retrieved data size: " << er.retrieved
+            << " actual data size: " << er.actual_size << endl;
+#else
+        cerr << "Error: Tried to convert \"" << er.data << "\" to a \""
+            << er.type_name << "\"." << endl;
+#endif
+#ifdef USE_STANDARD_EXCEPTION
+    }
+    catch (exception &er)
+    {
+        cerr << "Error: " << er.what() << endl;
+#endif
+    }
 }
